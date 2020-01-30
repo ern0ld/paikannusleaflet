c9ab6f86953db4ede8197f5a884fbc6a61653c5f
ern0ld <epa.suvila@gmail.com>
2020-01-30 15:35:44 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+let positionLat;
+let positionLong;
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("error");
+    }
+  }
+  
+  function showPosition(position) {
+      positionLat = position.coords.latitude;
+      positionLong = position.coords.longitude;
+      showLocation();
+   
+  }
+
+
+function showLocation(){
+//var mymap = L.map('mapid').setView([positionLat, positionLong], 13);
+
+var map = L.map('mapid').fitWorld();
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoiZXJuMGxkIiwiYSI6ImNrNjByMHI3ODA2c2IzbHFpc2lmemZrZTYifQ.hPQL649P0KTUqQhIAMLLPQ'
+
+}).addTo(map);
+var marker = L.marker([positionLat, positionLong]).addTo(map); 
+map.locate({setView: true, maxZoom: 16});
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
+}
+
+
+
+getLocation();
+//console.log(mymap);
